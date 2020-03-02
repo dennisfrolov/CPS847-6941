97ca3fc249463849d7527d3f5b70a128111a02f8
Dennis Frolov <37087835+dennisfrolov@users.noreply.github.com>
2020-03-02 17:40:46 -0500

    Create SixTimes.py

---
+++ SixTimes.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestMethods(unittest.TestCase):
+  def test_add(self):
+    seld.assertEqual(cps6941(8), 48)
+  def cps6941(y):
+    return (y*6);
+if __name__ == '__main__':
+    unittest.main()
