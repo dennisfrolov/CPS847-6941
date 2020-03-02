0e3267e2f50aa2b11983b9f1949b8b0f2bb89932
Dennis Frolov <37087835+dennisfrolov@users.noreply.github.com>
2020-03-02 17:45:36 -0500

    Update SixTimes.py

--- SixTimes.py
+++ SixTimes.py
@@ -1,9 +1,9 @@
 import unittest
 
 class TestMethods(unittest.TestCase):
-  def test_add(self):
-    self.assertEqual(cps6941(8), 48)
   def cps6941(y):
     return (y*6);
+  def test_add(self):
+    self.assertEqual(cps6941(8), 48)
 if __name__ == '__main__':
     unittest.main()
